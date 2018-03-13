ce40f2c247222474aee313af96afd1921e2b83de
MSFTserver <sawyerlemmo@gmail.com>
2018-03-12 17:46:34 -0700

    add supported fiats

--- bot/modules/price.js
+++ bot/modules/price.js
@@ -10,7 +10,7 @@ exports.commands = ['price'];
 exports.price = {
   usage: '<fiat/coin> <amount>',
   description:
-    'display price of RVN in specified alt coin/fiat from crypto compare\n**Example:** *!price USD 100*',
+    'display price of RVN in specified alt coin/fiat from crypto compare\n  **Example:** *!price USD 100*\n  **Supported Fiats:** *usd*, *eur*, *gbp*, *aud*, *brl*, *cad*, *chf*, *clp*, *cny*, *czk*, *dkk*, *hkd*, *huf*, *idr*, *ils*, *inr*, *jpy*, *krw*, *mxn*, *myr*, *nok*, *nzd*, *php*, *pkr*, *pln*, *rub*, *sek*, *sgd*, *thb*, *try*, *twd*, *zar* (case-insensitive)"',
   process: function(bot, msg, suffix) {
     let dt = new Date();
     let timestamp = moment()
